baf9bc93c653462ac4276db677205228123a829a
Prakrunya <75328132+Prakrunya@users.noreply.github.com>
2020-12-01 13:17:47 -0800

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -1,13 +1,22 @@
 #IMPORTS
 
 import pandas as pd
-import uo_puddles.uo_puddles as up
-import math
-import spacy, os
+import numpy as np
+import sys
+import os
+
 os.system('python -m spacy download en_core_web_md')
 import en_core_web_md
 nlp = en_core_web_md.load()
-import io
+def nlp_test(s):
+      return nlp(s)
+  
+my_github_name = 'uo-puddles'
+my_library_name = 'uo_puddles'
+clone_url = f'git clone https://github.com/{my_github_name}/{my_library_name}.git'  #create the url to get the library
+os.system(clone_url) # Cloning
+
+import uo_puddles.uo_puddles as up
 
 #KKN
 
